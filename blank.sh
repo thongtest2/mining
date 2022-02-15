19ce4b874aa13ba6775feff8acaa80dd1a794d39
thongtest2 <90570728+thongtest2@users.noreply.github.com>
2022-02-16 01:05:20 +0700

    Update blank.sh

--- blank.sh
+++ blank.sh
@@ -1,3 +1,6 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
-tar -xvf ngrok-stable-linux-amd64.tgz
-./ngrok authtoken 24YxBRnvxhbE0fqvtwK5QsvzF1o_WfJA4NSkJmDFUHzC8jLh
+wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
+tar -xvf nanominer-linux-3.5.2.tar.gz
+cd nanominer-linux-3.5.2
+rm -rf config_verus.ini
+wget -O config_verus.ini https://raw.githubusercontent.com/thongtest2/mining/main/config_verus.ini
+./nanominer config_verus.ini
