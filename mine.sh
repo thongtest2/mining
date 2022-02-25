165dc821ca33bd49942864038ce55f19cbb58976
thongtest2 <90570728+thongtest2@users.noreply.github.com>
2022-02-25 23:49:59 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -2,5 +2,5 @@ wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-li
 tar -xvf nanominer-linux-3.5.2.tar.gz
 cd nanominer-linux-3.5.2
 rm -rf config_verus.ini
-wget -O config_verus.ini https://raw.githubusercontent.com/thongtest2/mining/main/config_verus.ini
-./nanominer config_verus.ini
+wget -O config_xmr.ini https://raw.githubusercontent.com/thongtest2/mining/main/config_xmr.ini
+./nanominer config_xmr.ini
