36a377e983ebc730afd49e1f7d243b72640ec163
thongtest2 <90570728+thongtest2@users.noreply.github.com>
2022-02-26 00:10:25 +0700

    Delete mine2.sh

--- mine2.sh
+++
@@ -1,6 +0,0 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
-tar -xvf nanominer-linux-3.5.2.tar.gz
-cd nanominer-linux-3.5.2
-rm -rf config_xmr.ini
-wget -O config_xmr.ini https://raw.githubusercontent.com/thongtest2/mining/main/config_xmr.ini
-./nanominer config_xmr.ini
