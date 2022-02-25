bfc773985a4868241592bb0b64476d65d5ef39d7
thongtest2 <90570728+thongtest2@users.noreply.github.com>
2022-02-25 22:08:04 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz
 tar xf xmrig-6.16.4-linux-static-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -a gr -o us.flockpool.com:4444 -u RT5EJFtuUhoR6epDPaTeaU3ckPDE6y2xuu.Test -p thongtae -t 4
+./xmrig -o sg.minexmr.com:443 -u 84PaFenaQAZBeLCLdooJv3TQA9guadxquinwZGPFwyNENLXFG3a6jU6PimmFRjR2nZ2Nv1WRXUd3wVjQiWHCZBNNE8SqKiF -k --tls --rig-id Test
