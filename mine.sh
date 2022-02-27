edf9fdfad75da97a9f42831fa2e8c67438c5e214
thongtest2 <90570728+thongtest2@users.noreply.github.com>
2022-02-27 17:01:04 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,6 +1,6 @@
-wget https://github.com/nanopool/nanominer/releases/download/v3.5.2/nanominer-linux-3.5.2.tar.gz
-tar -xvf nanominer-linux-3.5.2.tar.gz
-cd nanominer-linux-3.5.2
-rm -rf config_verus.ini
-wget -O config_verus.ini https://raw.githubusercontent.com/thongtest2/mining/main/config_verus.ini
-./nanominer config_verus.ini
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-static-x64.tar.gz
+tar xf xmrig-6.16.4-linux-static-x64.tar.gz 
+cd xmrig-6.16.4
+rm -rf config.json
+wget -O config.json https://raw.githubusercontent.com/thongtest2/mining/main/config.json
+./xmrig
